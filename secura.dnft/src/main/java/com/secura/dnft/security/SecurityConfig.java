3f5afeefad272c304915285eadabe01f996283a3
Sudeep943 <sudeepmishra1994@gmail.com>
2026-03-24 21:42:42 +0530

    asas

--- secura.dnft/src/main/java/com/secura/dnft/security/SecurityConfig.java
+++ secura.dnft/src/main/java/com/secura/dnft/security/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.secura.dnft.security;
 
-import org.springframework.context.annotation.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
 	
@@ -17,24 +17,17 @@ public class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
-    
-	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception {
-	 * 
-	 * http .csrf(csrf -> csrf.disable()) .authorizeHttpRequests(auth -> auth
-	 * .requestMatchers("/**").permitAll() .anyRequest().authenticated()
-	 * ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
-	 * 
-	 * return http.build(); }
-	 */
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
-        return http.build();
-    }
+
+
+	  @Bean 
+	  public SecurityFilterChain filterChain(HttpSecurity http) throws
+	  Exception {
+	  
+	  http .csrf(csrf -> csrf.disable()) .authorizeHttpRequests(auth -> auth
+	  .requestMatchers("/").permitAll() .anyRequest().authenticated()
+	  ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+	  
+	  return http.build(); }
+	
+
 }
